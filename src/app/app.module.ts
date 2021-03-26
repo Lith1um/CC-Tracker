55c3f8a7acf636b6527454c2abd18ca3364862f4
Alex Rayner <alexrayner94@gmail.com>
2021-03-26 18:13:49 +0000

    set up dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 // Angular
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { AngularFireModule } from '@angular/fire';
 
 // Components
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+
+// env
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    // Firebase
+    AngularFireModule.initializeApp(environment.firebase),
+
+    StoreModule.forRoot({}, {}),
+
+    ReactiveFormsModule,
+    FormlyModule.forRoot()
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-GB'}
   ],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCdNWj6McP_pIJO-FSNpxrZ_LJxnXgS668",
+    authDomain: "cctracker-123e2.firebaseapp.com",
+    projectId: "cctracker-123e2",
+    storageBucket: "cctracker-123e2.appspot.com",
+    messagingSenderId: "952765623499",
+    appId: "1:952765623499:web:65e395dc97c719ac3cff4e",
+    measurementId: "G-NLGR4XLY4B"
+  }
 };
 
 /*
